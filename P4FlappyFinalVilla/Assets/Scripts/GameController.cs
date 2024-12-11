e3f45e11942a3284cdc742059545a3aa36a57932
dilpickle777 <727696@student.dusd.net>
2024-12-11 11:43:01 -0800

    added script
    added a sscript to repeat my background so it can keeep going on aand on

--- P4FlappyFinalVilla/Assets/Scripts/GameController.cs
+++ P4FlappyFinalVilla/Assets/Scripts/GameController.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
     public static GameController instance;
     public GameObject gameOverText;
     public bool gameOver = false;
+    public float scrollspeed = -1.5f;
+    public TextMeshProUGUI scoretext;
     // Start is called before the first frame update
     void Awake()
     {
@@ -24,7 +27,7 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!gameOver == true && Input.GetMouseButtonDown(0))
+        if ( gameOver == true && Input.GetMouseButtonDown(0))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
